ff2a0d7f1250a19db7a2663f8e3c710ac70bc727
Ponss <95647774+Ponss17@users.noreply.github.com>
2025-08-22 21:23:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def rango():
         puntos = current_data.get('ranking_in_tier', 'Desconocido')
         mmr_change = current_data.get('mmr_change_to_last_game', 'Desconocido')
 
-        respuesta = f"{rango} con {puntos} puntos 🤗✨, Mi última partida: [{mmr_change}]"
+        respuesta = f"Mi rango actual es {rango} con {puntos} puntos 🤗✨, Mi última partida: [{mmr_change}]"
     except Exception as e:
         print("Error:", e)
         respuesta = "Rango no disponible"
